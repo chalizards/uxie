18d1260e8e834ffc05775b962f4d0531de8f30f7
Brie <charmandercaramelo@gmail.com>
2023-10-04 20:31:11 -0300

    fix uxie script

--- uxie_script.rb
+++ uxie_script.rb
@@ -3,7 +3,112 @@
 require_relative 'lib/backup_manager'
 require_relative 'lib/plan'
 
-plan_name, user_date, plan_start_date = ARGV
+plan_name, user_date = ARGV
 
-backup = BackupManager.new(plan_name, user_date, plan_start_date)
-backup.expired?
+BackupManager.new(plan_name, user_date).keep_backup?
+
+# 2023-10-03
+# BackupManager.new('beginner', '2023-10-03').keep_backup?
+
+# BackupManager.new('pro', '2023-10-03').keep_backup?
+
+# BackupManager.new('ultra', '2023-10-03').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2023-08-22
+# BackupManager.new('beginner', '2023-08-22').keep_backup?
+
+# BackupManager.new('pro', '2023-08-22').keep_backup?
+
+# BackupManager.new('ultra', '2023-08-22').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2023-08-21
+# BackupManager.new('beginner', '2023-08-21').keep_backup?
+
+# BackupManager.new('pro', '2023-08-21').keep_backup?
+
+# BackupManager.new('ultra', '2023-08-21').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2023-07-31
+# BackupManager.new('beginner', '2023-07-31').keep_backup?
+
+# BackupManager.new('pro', '2023-07-31').keep_backup?
+
+# BackupManager.new('ultra', '2023-07-31').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2023-06-30
+# BackupManager.new('beginner', '2023-06-30').keep_backup?
+
+# BackupManager.new('pro', '2023-06-30').keep_backup?
+
+# BackupManager.new('ultra', '2023-06-30').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2022-12-31
+# BackupManager.new('beginner', '2022-12-31').keep_backup?
+
+# BackupManager.new('pro', '2022-12-31').keep_backup?
+
+# BackupManager.new('ultra', '2022-12-31').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2022-10-31
+# BackupManager.new('beginner', '2022-10-31').keep_backup?
+
+# BackupManager.new('pro', '2022-10-31').keep_backup?
+
+# BackupManager.new('ultra', '2022-10-31').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2022-09-30
+# BackupManager.new('beginner', '2022-09-30').keep_backup?
+
+# BackupManager.new('pro', '2022-09-30').keep_backup?
+
+# BackupManager.new('ultra', '2022-09-30').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2021-12-31
+# BackupManager.new('beginner', '2021-12-31').keep_backup?
+
+# BackupManager.new('pro', '2021-12-31').keep_backup?
+
+# BackupManager.new('ultra', '2021-12-31').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2021-12-30
+# BackupManager.new('beginner', '2021-12-30').keep_backup?
+
+# BackupManager.new('pro', '2021-12-30').keep_backup?
+
+# BackupManager.new('ultra', '2021-12-30').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2016-12-31
+# BackupManager.new('beginner', '2016-12-31').keep_backup?
+
+# BackupManager.new('pro', '2016-12-31').keep_backup?
+
+# BackupManager.new('ultra', '2016-12-31').keep_backup?
+
+#------------------------------------------------------------#
+
+# 2015-12-31
+# BackupManager.new('beginner', '2015-12-31').keep_backup?
+
+# BackupManager.new('pro', '2015-12-31').keep_backup?
+
+# BackupManager.new('ultra', '2015-12-31').keep_backup?
